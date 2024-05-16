304091e02ed3a17dcb512024bf9fafef9a4f62df
ahmedbilal <ahmed258bilal@gmail.com>
2024-05-16 13:21:59 +0500

    commit after designing UI of game

--- script.js
+++ script.js
@@ -1,77 +1,91 @@
 
-    // generating choice from computer
+const rock = document.querySelector('#btn1');
+rock.addEventListener('click', () => {
+    let humanSelection = getHumanChoice(rock.value);
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
 
-    alert("Open console in your browser to play game");
-    function getComputerChoice() {
-        var comp_choice = ["rock", "paper", "scissor"];
-        var randomIndex = Math.floor(Math.random() * comp_choice.length);
-        var computer_choice = comp_choice[randomIndex]; // Store the choice in a variable
-        console.log("Computer choice is: " + computer_choice); // Log the computer's choice
-        return computer_choice; // Return the computer's choice
+});
+
+const paper = document.querySelector('#btn2');
+paper.addEventListener('click', () => {
+    let humanSelection = getHumanChoice(paper.value);
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+});
+
+const scissor = document.querySelector('#btn3');
+scissor.addEventListener('click', () => {
+    let humanSelection = getHumanChoice(scissor.value);
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+});
+
+function getHumanChoice(choice) {
+   
+    document.getElementById("round").innerHTML= "Round : " + roundCounter;
+    if (choice === rock.value || choice === paper.value || choice === scissor.value) {
+        document.getElementById("human").innerHTML="Your choice is: " + choice;
+        return choice;
+    } else {
+        console.log("Sorry! You entered a wrong choice.");
+        return null;
     }
-
-
- // Function to get human's choice
- function getHumanChoice() {
-        let human_choice = prompt("Enter your choice in game: Rock, Paper, or Scissor").toLowerCase();
-        if (human_choice == "rock" || human_choice == "paper" || human_choice == "scissor") {
-            console.log("Your choice is: " + human_choice);
-            return human_choice;
-        } else {
-            console.log("Sorry! You entered a wrong choice.");
-            return null;
-        }
-    }
-
-
-//  initilizing score
+}
+function getComputerChoice() {
+    var comp_choice = ["rock", "paper", "scissor"];
+    var randomIndex = Math.floor(Math.random() * comp_choice.length);
+    var computer_choice = comp_choice[randomIndex]; // Store the choice in a variable
+    document.getElementById("computer").innerHTML="Computer choice is: " + computer_choice;
+    return computer_choice; // Return the computer's choice
+}
+var roundCounter = 1;
 var humanScore = 0;
 var computerScore = 0;
-function playRound(humanSelection, computerSelection) {
-        if (humanSelection === computerSelection) {
-            console.log("The Games tie! \n  Play Again!");
-        } else if (
-            (humanSelection == "paper" && computerSelection == "rock") ||
-            (humanSelection == "rock" && computerSelection == "scissor") ||
-            (humanSelection == "scissor" && computerSelection == "paper")
-        ) {
-            console.log("You win! as : ", humanSelection , "beats", computerSelection);
-            console.log("Your's score=",humanScore=  humanScore+1);
-            console.log("computer's score=",computerScore= computerScore);
 
-        }
-        else if (
-            (computerSelection == "paper" && humanSelection == "rock") ||
-            (computerSelection == "rock" && humanSelection == "scissor") ||
-            (computerSelection == "scissor" && humanSelection == "paper")
-        ) {
-            console.log("Computer wins! as : ", computerSelection , "beats", humanSelection);
-            console.log("Your's score=",humanScore=  humanScore);
-            console.log("computer's score=",computerScore= computerScore+1);
-        }
-        else {
-            console.log("Computer wins!");
-        }
-    }
-    let round=1;
-   
-    for (let round=1; round <= 5; round++) {
-         console.log("---------------Round no#", round , "-----------------");
-        let humanSelection = getHumanChoice();
-    if (humanSelection !== null) {
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    else{
-        console.log("you did not enter any choice");
-    }
+
+
+function playRound(humanSelection, computerSelection) {
+    if (roundCounter >= 5 ) {
         
+         document.getElementById("end").innerHTML="You've played 5 rounds. Game Over!";
+        let buttons = document.querySelectorAll("button");
+        buttons.forEach(button => {
+            button.disabled = true;
+        })
     }
-    console.log("Total Result:");
-    if(computerScore>humanScore){
-        console.log("After 5 rounds, computer is winner of this game. Computer score is  ",computerScore );
+
+    roundCounter++;
+    if (humanSelection === computerSelection) {
+
+        document.getElementById("rule").innerHTML="The Games tie! ";
+    } else if (
+        (humanSelection == "paper" && computerSelection == "rock") ||
+        (humanSelection == "rock" && computerSelection == "scissor") ||
+        (humanSelection == "scissor" && computerSelection == "paper")
+    ) {
+        
+        document.getElementById("rule").innerHTML=   humanSelection + " beats " + computerSelection;
+        document.getElementById("humanscore").innerHTML="Your's score="+ (humanScore=humanScore+1);
+        document.getElementById("computerscore").innerHTML="computer's score="+ computerScore;
     }
-    else{
-        console.log("After 5 rounds, you are the winner. your score is : ", humanScore);
-    }
+    else if (
+        (computerSelection == "paper" && humanSelection == "rock") ||
+        (computerSelection == "rock" && humanSelection == "scissor") ||
+        (computerSelection == "scissor" && humanSelection == "paper")
+    ) {
     
+        document.getElementById("rule").innerHTML= computerSelection+ " beats "+ humanSelection;
+        document.getElementById("humanscore").innerHTML="Your's score="+ humanScore ;
+        document.getElementById("computerscore").innerHTML="computer's score="+ (computerScore=computerScore+1);
+    }
+
+    else {
+        console.log("Nothing happens");
+    }
+
+
+}
+
+
+
